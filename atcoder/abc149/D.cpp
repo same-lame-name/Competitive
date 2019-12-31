0f54235a2cbc0b768a67a98d9029da89b5520460
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2019-12-31 15:17:00 +0530

    Add solution for problem `D - Prediction and Restriction` Link: https://atcoder.jp/contests/abc149/tasks/abc149_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc149/submissions/9257037

--- atcoder/abc149/D.cpp
+++ atcoder/abc149/D.cpp
@@ -1,53 +1,63 @@
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+#define PRECISION(x) cout << fixed << setprecision(x); 
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(i,n) for (int i = 0; i < n; ++i)
+#define DO(X) while(X--)
+#define DRI(X) int (X); cin >> X
+#define DRII(X, Y) int X, Y; cin >> X >> Y
+#define DRIII(X, Y, Z) int X, Y, Z; cin >> X >> Y >> Z
+#define read(X) for(int &ele : (X)) cin >> ele
+#define readI(X) for(int idx = 1; idx < SZ((X)); ++idx) cin >> (X)[idx]
+#define debug(x) cerr << '>' << #x << ':' << x << endl
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+
 using namespace std;
-#define int 		  long long
-#define	pb 			  push_back
-#define all(a)		  (a).begin(),(a).end()
-#define sz(x)		  (int)x.size()
-#define endl	 	  "\n"
-#define inf 		  1000000007
-#define FOR(i, a, b)  for(int i=a; i<b; i++)
-int dp[100011] = {0};
-int r, p, s;
-int fu(char st){
-	if(st=='r')return p;
-	else if(st=='p') return s;
-	else return r;
-}
-void solve(){
-	int n, k, res=0;
-	cin>>n>>k;
-	cin>>r>>s>>p;
-	string g;
-	cin>>g;
-	FOR(i, 0, k){
-		res+=fu(g[i]);
-		dp[i] = res;
-	}
-	//cout<<dp[k-1]<<endl;
-	FOR(i, k, sz(g)){
-		if(g[i-k]==g[i]){
+typedef long long LL;
+const int INF = 1<<29;
+inline int in() { int x; cin >> x; return x; }
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
 
-			dp[i] = dp[i-1];
-			g[i]='0';
+int main(){
+	FAST_IO 
+	DRII(N, K);
+	vector<int> gain(3);
+	vector<vector<int>> group(K);
+	REP(rep, 3) cin >> gain[rep];
+	string moves; cin >>  moves;
+	for(int idx = 0; idx < N; ++idx) group[idx % K].push_back(idx);
+	int maxProfit = 0;
+	auto winning = [&](char a, char b){
+		if(a == 'r' && b== 's'){
+			return gain[0];
 		}
-		else{
-			dp[i] = dp[i-1]+fu(g[i]);
+		if(a == 's' && b == 'p'){
+			return gain[1];
 		}
-		//cout<<dp[i]<<" ";
+		if(a == 'p' && b == 'r'){
+			return gain[2];
+		}
+		return 0;
+	};
+	for(int modF = 0; modF < K; ++modF){
+		int M = SZ(group[modF]);
+		vector<vector<int>> curProfit(M, vector<int>(3));
+		for(int idx = 0; idx < M; ++idx){
+			curProfit[idx][0] = (idx > 0 ? max(curProfit[idx - 1][1], curProfit[idx - 1][2]) : 0)
+								+ winning('r', moves[group[modF][idx]]); 
+			curProfit[idx][1] = (idx > 0 ? max(curProfit[idx - 1][0], curProfit[idx - 1][2]) : 0)
+								+ winning('s', moves[group[modF][idx]]); 
+			curProfit[idx][2] = (idx > 0 ? max(curProfit[idx - 1][1], curProfit[idx - 1][0]) : 0)
+								+ winning('p', moves[group[modF][idx]]); 
+		}
+		maxProfit += max({curProfit[M - 1][0], curProfit[M - 1][1], curProfit[M - 1][2]});
 	}
-	cout<<dp[n-1];
-}
-
-signed main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int t=1;
-	//cin>>t;
-	FOR(i, 1, t+1){	
-	//	cout<<"Case #"<<i<<":"<<endl;
-		solve();
-	}
+	cout << maxProfit << endl;
 	return 0;
- 
 }
