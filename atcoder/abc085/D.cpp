0b1fa1098c35dd4704c83f14d192aedcd8258a79
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-04-16 21:47:00 +0530

    Add solution for problem `D - Katana Thrower` Link: https://atcoder.jp/contests/abc085/tasks/abc085_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc085/submissions/11991250

---
+++ atcoder/abc085/D.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+
+int main(){
+  FAST_IO  
+  int N, h, trial = 0, best = 0; cin >> N >> h;
+  vector<int> onetime;
+
+  for(int rep = 0; rep < N; ++rep){
+    int a, b; cin >> a >> b;
+    max_self(best, a);
+    onetime.PB(b);
+  }
+  sort(ALLR(onetime));
+
+  for(int ele : onetime){
+    if(ele <= best || h <= 0) break;
+    h -= ele;
+    trial++;
+  }
+
+  if(h > 0) trial += (h / best) + (h % best > 0);
+
+  cout << trial << "\n";
+  return 0;
+}
