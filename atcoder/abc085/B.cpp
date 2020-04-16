d60b0a6b003a6047a9963fd6f449515d2ff86a03
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-04-16 20:30:00 +0530

    Add solution for problem `B - Kagami Mochi` Link: https://atcoder.jp/contests/abc085/tasks/abc085_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc085/submissions/11989535

---
+++ atcoder/abc085/B.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+
+int main(){
+  FAST_IO  
+  
+  int N, count = 0; cin >> N;
+  vector<int> diameter(N);
+  for(int &ele : diameter) cin >> ele;
+
+  sort(ALL(diameter));
+  for(int idx = 1; idx < N; ++idx){
+    count += (diameter[idx] != diameter[idx - 1]);
+  }
+  cout << count + 1 << "\n";
+  return 0;
+}
