22c6d79eef34794bccd4335f90d78ecca6c3ef41
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-04-20 12:09:00 +0530

    Add solution for problem `B - Card Game for Two` Link: https://atcoder.jp/contests/abc088/tasks/abc088_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc088/submissions/12163280

---
+++ atcoder/abc088/B.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+
+int main(){
+  FAST_IO  
+
+  int N, diff = 0;
+  cin >> N;
+  vector<int> card(N);
+  for(int &ele : card) cin >> ele;
+
+  sort(ALLR(card));
+  for(int idx = 0; idx < N; ++idx){
+    if(idx & 1) diff -= card[idx];
+    else diff += card[idx];
+  }
+
+  cout << diff << "\n";
+  return 0;
+}
