3fe199f047d77d62a86a4a4d890c3bfc85a3849c
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-05-02 17:52:00 +0530

    Add solution for problem `A - Two Coins` Link: https://atcoder.jp/contests/abc091/tasks/abc091_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc091/submissions/12597672

---
+++ atcoder/abc091/A.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+
+int main(){
+  FAST_IO  
+
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if(a + b >= c) cout << "Yes\n";
+  else cout << "No\n";
+  return 0;
+}
