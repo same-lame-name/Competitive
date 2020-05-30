8fa0fe73787dcd4d4f1aedb4031d7587521c680b
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-05-30 19:25:00 +0530

    Add solution for problem `A - Painting` Link: https://atcoder.jp/contests/keyence2020/tasks/keyence2020_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/keyence2020/submissions/13741492

---
+++ atcoder/keyence2020/A.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int H, W, N;
+
+int main(){
+  FAST_IO
+  cin >> H >> W >> N;
+  if(H < W) swap(H, W);
+
+  cout << N / H + (N % H > 0) << '\n';
+  return 0;
+}
