62ff808ba418480267eb801ba7449b8540874c3d
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-06-15 15:47:00 +0530

    Add solution for problem `C - Minimization` Link: https://atcoder.jp/contests/abc101/tasks/arc099_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc101/submissions/14387846

---
+++ atcoder/abc101/C.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> void max_self(T & a, const T & b) { if(a < b) a = b; }
+template<class T> void min_self(T & a, const T & b) { if(a > b) a = b; }
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int N, k;
+
+
+
+int main(){
+  FAST_IO
+  
+  cin >> N >> k;
+  for(int rep = 1, in; rep <= N; ++rep){
+    cin >> in;
+  }
+
+  cout << (N + k - 3) / (k - 1) << '\n';
+  
+  return 0;
+}
