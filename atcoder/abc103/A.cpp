bec392d4ff3b6c8302713f41a4f94192e6bb8dab
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-06-21 13:35:00 +0530

    Add solution for problem `A - Task Scheduling Problem` Link: https://atcoder.jp/contests/abc103/tasks/abc103_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc103/submissions/14520204

---
+++ atcoder/abc103/A.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> bool max_self(T & a, const T & b){ 
+  return b > a ? a = b, true : false; 
+}
+template<class T> bool min_self(T & a, const T & b){ 
+  return b < a ? a = b, true : false;    
+}
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int main(){
+  FAST_IO
+  vector<int> num(3);
+  for(int &ele : num) cin >> ele;
+  sort(ALL(num));
+  
+  int sum = 0;
+  for(int idx = 1; idx < 3; ++idx) sum += num[idx] - num[idx - 1];
+  cout << sum << '\n';
+  return 0;
+}
