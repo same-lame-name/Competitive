580ec3c96124e96b5d75ab72e6981b76a4e8b7a1
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-06-21 16:42:00 +0530

    Add solution for problem `C - Two Teams Composing` Link: https://codeforces.com/contest/1335/problem/C Tags: binary search, greedy, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1335/submission/84577406

---
+++ codeforces/1335/C.cpp
@@ -0,0 +1,59 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> bool max_self(T & a, const T & b){ 
+  return b > a ? a = b, true : false; 
+}
+template<class T> bool min_self(T & a, const T & b){ 
+  return b < a ? a = b, true : false;    
+}
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int testcases, N, most, uniq;
+map<int, int> cnt;
+
+void solve(){
+  cnt.clear();
+  most = uniq= 0;
+
+  for(int rep = 0, x; rep < N; ++rep){
+    cin >> x;
+    if(!cnt.count(x)) uniq++;
+    cnt[x]++;
+  }
+
+  for(auto &[k, v] : cnt){
+    max_self(most, v);
+  }
+
+  cout << max(min(uniq - 1, most), min(most - 1, uniq)) << '\n';
+
+  return;
+}
+  
+  
+
+int main(){
+  FAST_IO
+  cin >> testcases;
+  while(testcases--){
+    cin >> N;
+    solve();
+  }
+
+  return 0;
+}
