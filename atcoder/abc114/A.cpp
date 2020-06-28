339d85b91a6576c6bcdde5db671ceef8a03180ce
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-06-28 21:05:00 +0530

    Add solution for problem `A - 753` Link: https://atcoder.jp/contests/abc114/tasks/abc114_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc114/submissions/14826309

---
+++ atcoder/abc114/A.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> bool max_self(T & a, const T & b){ 
+  return b > a ? a = b, true : false; 
+}
+template<class T> bool min_self(T & a, const T & b){ 
+  return b < a ? a = b, true : false;    
+}
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int main(){
+  FAST_IO
+  int N;
+  cin >> N;
+  if(N == 3 || N == 5 || N == 7) cout << "YES\n";
+  else cout << "NO\n";
+  return 0;
+}
+
