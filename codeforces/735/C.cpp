d0e7355bc873b05ac3796639211659d61c06f8ee
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-07-01 14:14:00 +0530

    Add solution for problem `C - Tennis Championship` Link: https://codeforces.com/contest/735/problem/C Tags: combinatorics, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/735/submission/85605568

---
+++ codeforces/735/C.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> bool max_self(T & a, const T & b){ 
+  return b > a ? a = b, true : false; 
+}
+template<class T> bool min_self(T & a, const T & b){ 
+  return b < a ? a = b, true : false;    
+}
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+LL fib[105], N;
+int rounds = 1;
+
+int main(){
+  FAST_IO
+  fib[0] = 1;
+  fib[1] = 2;
+  cin >> N;
+  
+  for(int r = 2; ; ++r){
+    fib[r] = fib[r - 1] + fib[r - 2];
+    if(fib[r] > N) break;
+    rounds = r;
+  }
+
+  cout << rounds << '\n';
+  return 0;
+}
+
