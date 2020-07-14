c544999750c39df3894c75ac93561fe89063cdc7
Shashank Tiwari <ts.shashank.tiwari@gmail.com>
2020-07-14 19:18:00 +0530

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86835832

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+#define PRECISION(x) cout << fixed << setprecision(x)
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define ALLR(X) (X).rbegin(), (X).rend()
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+
+using namespace std;
+ 
+template<class T> bool max_self(T & a, const T & b){ 
+  return b > a ? a = b, true : false; 
+}
+template<class T> bool min_self(T & a, const T & b){ 
+  return b < a ? a = b, true : false;    
+}
+typedef long long LL;
+const int INF = 1e9 + 7;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+
+int N, testcases;
+
+int main(){
+  FAST_IO
+
+  cin >> testcases;
+  while(testcases--){
+    cin >> N;
+    for(int rep = 0; rep < N; ++rep) cout << "1 ";
+    cout << '\n';
+  }
+
+  return 0;
+}
+
